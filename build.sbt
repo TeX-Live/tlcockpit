93b554c99d805535dbb5c44cb25feddd0da753bc
Norbert Preining <norbert@preining.info>
2017-09-26 11:05:27 +0900

    switch to Future/async tlmgr calls

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "tlcockpit"
 
-version := "0.1"
+version := "0.2"
 
 scalaVersion := "2.12.3"
 
--- src/main/scala/TLCockpit/ApplicationMain.scala
+++ src/main/scala/TLCockpit/ApplicationMain.scala
@@ -6,21 +6,13 @@
 
 package TLCockpit
 
-// import java.util.Date
-
-// import java.io.InputStream
-// import java.io.OutputStream
-// import java.io.InputStreamReader
-// import java.io.OutputStreamWriter
-// import java.io.BufferedWriter
-// import java.io.BufferedReader
-
 import TeXLive.{TLPackage, TlmgrProcess}
 
-// import scalafx.beans.property.ObjectProperty
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 import scalafx.geometry.HPos
 import scalafx.scene.control.Alert.AlertType
-// import scalafx.scene.input.MouseButton._
 
 // needed see https://github.com/scalafx/scalafx/issues/137
 import scalafx.scene.control.TableColumn._
@@ -32,22 +24,10 @@ import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.layout._
 import scalafx.scene.control._
-// import scalafx.scene.text._
 import scalafx.event.ActionEvent
 import scalafx.collections.ObservableBuffer
-// import scalafx.beans.property.StringProperty
-// import scalafx.scene.input.{KeyCode, KeyEvent, MouseEvent, MouseButton}
-// import scalafx.stage.Popup
-
-//import com.sun.xml.internal.bind.WhiteSpaceProcessor
-
 import scala.sys.process._
-// import scala.concurrent.SyncVar
 import scala.collection.mutable.ArrayBuffer
-// import scala.collection.mutable.ArrayBuilder
-
-// import scala.io.Source
-//import scala.swing._
 
 
 object ApplicationMain extends JFXApp {
@@ -114,11 +94,15 @@ object ApplicationMain extends JFXApp {
       }
     })
 
-  def tlmgr_command(s: String): Array[String] = {
+  def tlmgr_async_command(s: String, f: Array[String] => Unit): Unit = {
     errorfield.text = ""
     outputfield.text = ""
     outerrpane.expanded = false
-    tlmgr.send_command(s)
+    val foo = Future { tlmgr.send_command(s) }
+    foo onComplete {
+      case Success(ret) => f(ret)
+      case Failure(t) => println("An ERROR has occurred: " + t.getMessage)
+    }
   }
 
   def update_update_button_state(): Unit = {
@@ -138,41 +122,46 @@ object ApplicationMain extends JFXApp {
     sys.exit(0)
   }
 
-  def callback_load(s: String): Unit = {
-    val foo = tlmgr_command("load " + s)
+  def callback_load_update_info(s:String, strs: Array[String]): Unit = {
     if (s == "remote") {
-      val pkglines: Array[String] = tlmgr_command("info --data name,localrev,remoterev,shortdesc")
-      val newpkgs = ArrayBuffer[TLPackage]()
-      pkglines.map(line => {
-        val fields: Array[String] = line.split(",", -1)
-        val sd = fields(3)
-        val shortdesc = if (sd.isEmpty) "" else sd.substring(1).dropRight(1).replace("""\"""",""""""")
-        newpkgs += new TLPackage(fields(0), fields(1), fields(2), shortdesc)
+      tlmgr_async_command("info --data name,localrev,remoterev,shortdesc", (s: Array[String]) => {
+        val newpkgs = ArrayBuffer[TLPackage]()
+        s.map((line:String) => {
+          val fields: Array[String] = line.split(",", -1)
+          val sd = fields(3)
+          val shortdesc = if (sd.isEmpty) "" else sd.substring(1).dropRight(1).replace("""\"""",""""""")
+          newpkgs += new TLPackage(fields(0), fields(1), fields(2), shortdesc)
+        })
+        pkgs.clear()
+        newpkgs.map(pkgs += _)
+        viewpkgs.clear()
+        pkgs.map(viewpkgs += _)
+        // check for updates available
+        update_update_button_state()
       })
-      pkgs.clear()
-      newpkgs.map(pkgs += _)
-      viewpkgs.clear()
-      pkgs.map(viewpkgs += _)
-      // check for updates available
-      update_update_button_state()
-     }
-    // println("got result from load " + s + ": ")
-    // foo.map(println(_))
+    }
+  }
+
+  def callback_load(s: String): Unit = {
+    tlmgr_async_command("load " + s, callback_load_update_info(s, _) )
   }
 
   def callback_run_text(s: String): Unit = {
-    tlmgr_command(s)
+    tlmgr_async_command(s, (s: Array[String]) => {})
+    // tlmgr_command(s)
   }
   def callback_run_cmdline(): Unit = {
     callback_run_text(cmdline.text.value)
   }
 
   def callback_list_collections(): Unit = {
-    val foo = tlmgr_command("info collections")
-    // println("got result from info collections: ")
-    // foo.map(println(_))
-    outputfield.text = foo.mkString("\n")
-    errorfield.text = tlmgr.errorBuffer.toString
+    tlmgr_async_command("info collections", foo => {
+      val foo: Array[String] = Array()
+      // println("got result from info collections: ")
+      // foo.map(println(_))
+      outputfield.text = foo.mkString("\n")
+      errorfield.text = tlmgr.errorBuffer.toString
+    })
   }
 
   def not_implemented_info(): Unit = {
@@ -185,14 +174,18 @@ object ApplicationMain extends JFXApp {
   }
 
   def callback_run_external(s: String): Unit = {
-    outputfield.text = s.!!
+    val foo = Future { s.!! }
+    foo.onComplete {
+      case Success(ret) => outputfield.text = ret.mkString("\n")
+      case Failure(t) => println("An ERROR has occurred: " + t.getMessage)
+    }
   }
 
   def callback_about(): Unit = {
     new Alert(AlertType.Information) {
       initOwner(stage)
       title = "About TLCockpit"
-      headerText = "TLCockpit version 0.1\nLicense: GPL3+"
+      headerText = "TLCockpit version 0.2\nLicense: GPL3+"
       contentText = "Brought to you by Norbert\nSources: https://github.com/TeX-Live/tlcockpit"
     }.showAndWait()
   }
@@ -216,50 +209,57 @@ object ApplicationMain extends JFXApp {
     newpkgs.map(viewpkgs += _)
   }
   def callback_update_all() : Unit = {
-    val output = tlmgr_command(s"update --all")
-    update_update_button_state()
-    callback_show_all()
+    tlmgr_async_command("update --all", s => {
+      update_update_button_state()
+      callback_show_all()
+    })
   }
   def callback_update_self() : Unit = {
-    val output = tlmgr_command(s"update --self")
     // TODO
     // should we restart tlmgr here - it might be necessary!!!
-    update_update_button_state()
-    callback_show_all()
+    tlmgr_async_command("update --self", s => {
+      update_update_button_state()
+      callback_show_all()
+    })
   }
+
 
   def callback_show_pkg_info(pkg: String): Unit = {
-    val pkginfo = tlmgr_command(s"info $pkg")
-    val dialog = new Dialog() {
-      initOwner(stage)
-      title = s"Package Information for $pkg"
-      headerText = s"Package Information for $pkg"
-      resizable = true
-    }
-    dialog.dialogPane().buttonTypes = Seq(ButtonType.OK)
-    val grid = new GridPane() {
-      hgap = 10
-      vgap = 10
-      padding = Insets(20)
-    }
-    var crow = 0
-    pkginfo.foreach((line:String) => {
-      val keyval = line.split(":",2).map(_.trim)
-      if (keyval.length == 2) {
-        val keylabel = new Label(keyval(0))
-        val vallabel = new Label(keyval(1))
-        vallabel.wrapText = true
-        grid.add(keylabel, 0, crow)
-        grid.add(vallabel, 1, crow)
-        crow += 1
+    // val pkginfo = tlmgr_command(s"info $pkg")
+    tlmgr_async_command(s"info $pkg", pkginfo => {
+      // need to call runLater to go back to main thread!
+      Platform.runLater {
+        val dialog = new Dialog() {
+          initOwner(stage)
+          title = s"Package Information for $pkg"
+          headerText = s"Package Information for $pkg"
+          resizable = true
+        }
+        dialog.dialogPane().buttonTypes = Seq(ButtonType.OK)
+        val grid = new GridPane() {
+          hgap = 10
+          vgap = 10
+          padding = Insets(20)
+        }
+        var crow = 0
+        pkginfo.foreach((line: String) => {
+          val keyval = line.split(":", 2).map(_.trim)
+          if (keyval.length == 2) {
+            val keylabel = new Label(keyval(0))
+            val vallabel = new Label(keyval(1))
+            vallabel.wrapText = true
+            grid.add(keylabel, 0, crow)
+            grid.add(vallabel, 1, crow)
+            crow += 1
+          }
+        })
+        grid.columnConstraints = Seq(new ColumnConstraints(100, 150, 200), new ColumnConstraints(100, 300, 5000, Priority.Always, new HPos(HPos.Left), true))
+        dialog.dialogPane().content = grid
+        dialog.width = 500
+        dialog.showAndWait()
       }
     })
-    grid.columnConstraints = Seq(new ColumnConstraints(100,150,200),new ColumnConstraints(100,300,5000,Priority.Always,new HPos(HPos.Left),true))
-    dialog.dialogPane().content = grid
-    dialog.width = 500
-    dialog.showAndWait()
   }
-
 
   var testmode = false
   if (parameters.unnamed.nonEmpty) {
@@ -457,28 +457,25 @@ object ApplicationMain extends JFXApp {
     }
   }
 
-  /* val startalert = new Alert(AlertType.Information)
+  val startalert = new Alert(AlertType.Information)
   startalert.setTitle("Loading package database ...")
   startalert.setContentText("Loading package database, this might take a while. Please wait!")
   startalert.show()
-  */
 
   // load the initial set of packages
-  val pkglines = if (testmode)
-    Array("aa,0,1,ffobar", "bb,4,5,fafaf")
-  else
-    tlmgr_command("info --only-installed --data name,localrev,shortdesc")
+  var pkglines: Array[String] = Array()
 
-
-  pkglines.map(line => {
-    val fields: Array[String] = line.split(",",-1)
-    val sd = if (fields(2).isEmpty) "" else fields(2).substring(1).dropRight(1).replace("""\"""",""""""")
-    pkgs += new TLPackage(fields(0),fields(1),"0",sd)
+  tlmgr_async_command("info --only-installed --data name,localrev,shortdesc", (s: Array[String]) => {
+    s.map(line => {
+      val fields: Array[String] = line.split(",", -1)
+      val sd = if (fields(2).isEmpty) "" else fields(2).substring(1).dropRight(1).replace("""\"""",""""""")
+      pkgs += new TLPackage(fields(0), fields(1), "0", sd)
+    })
+    viewpkgs.clear()
+    pkgs.map(viewpkgs += _)
+    Platform.runLater { startalert.close() }
   })
-  viewpkgs.clear()
-  pkgs.map(viewpkgs += _)
 
-  // startalert.close()
 
 }  // object ApplicationMain
 
