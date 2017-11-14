7d87e0d9c4af6c4693d5963d37b3d4f0c86c4c57
Norbert Preining <norbert@preining.info>
2017-11-14 15:53:02 +0900

    forgot missing files

---
+++ src/main/scala/TLCockpit/PaperDialog.scala
@@ -0,0 +1,83 @@
+package TLCockpit
+
+import TLCockpit.ApplicationMain.stage
+import TeXLive.TLPaperConf
+
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
+import scalafx.geometry.{HPos, Insets}
+import scalafx.scene.control._
+import scalafx.scene.layout._
+
+class PaperDialog(paperconf: Map[String, TLPaperConf])  {
+
+  case class Result(selected: Map[String,String])
+
+  val dialog = new Dialog[Result]() {
+    initOwner(stage)
+    title = s"Paper configuration"
+    headerText = s"Paper configuration"
+    resizable = true
+  }
+  dialog.dialogPane().buttonTypes = Seq(ButtonType.OK, ButtonType.Cancel)
+  val grid = new GridPane() {
+    hgap = 10
+    vgap = 10
+    padding = Insets(20)
+  }
+
+  var crow = 0
+
+  def update_all_to(str: String): Unit = {
+    cbseq.foreach(f => f._2.value = str)
+  }
+  grid.add(new Label("Set all to:"), 0, crow)
+  grid.add(new HBox(20) {
+    children = List(
+      new Button("a4") { onAction = _ => update_all_to("a4") },
+      new Button("letter") { onAction = _ => update_all_to("letter")}
+    )}, 1, crow)
+  crow += 1
+  def do_one(k: String, v: ChoiceBox[String], row: Int): Int = {
+    grid.add(new Label(k), 0, row)
+    grid.add(v, 1, row)
+    row + 1
+  }
+
+  val cbseq: Map[String, ChoiceBox[String]] = paperconf.map { p =>
+    val prog: String = p._1
+    val opts: TLPaperConf = p._2
+    val paperopts: Seq[String] = opts.options
+    val cb = new ChoiceBox[String](ObservableBuffer(paperopts.sorted))
+    cb.value = paperopts.head
+    crow = do_one(prog,cb, crow)
+    (prog, cb)
+  }
+
+  grid.columnConstraints = Seq(new ColumnConstraints(100, 100, 150), new ColumnConstraints(150, 150, 5000, Priority.Always, new HPos(HPos.Left), true))
+  dialog.dialogPane().content = grid
+  dialog.width = 300
+  dialog.height = 1500
+
+  dialog.resultConverter = dialogButton =>
+    if (dialogButton == ButtonType.OK)
+      Result(cbseq.mapValues(_.value.value))
+    else
+      null
+
+  def showAndWait(): Option[Map[String,String]] = {
+    val result = dialog.showAndWait()
+
+    result match {
+      case Some(Result(foo)) =>
+        // println("Got resutl " + foo)
+        Some(foo)
+      case Some(foo) =>
+        // println("Got strange result " + foo)
+        None
+      case None =>
+        None
+    }
+  }
+
+}
---
+++ src/main/scala/TLCockpit/TLDisplayClasses.scala
@@ -0,0 +1,30 @@
+package TLCockpit
+
+import scalafx.beans.property.{ObjectProperty, StringProperty}
+
+
+// Note!!! we have to use ObjectProperty[Int] here instead of IntegerProperty
+// since IntegerProperty does NOT implement Observable[Int,Int]
+// see https://github.com/scalafx/scalafx/issues/243
+case class TLPackageDisplay(name: StringProperty, lrev: ObjectProperty[Int], rrev: ObjectProperty[Int],
+                            shortdesc: StringProperty, size: ObjectProperty[Int], installed: StringProperty) {
+  def this(_name: String, _lrev: String, _rrev: String, _shortdesc: String, _size: String, _installed: String) =
+    this(
+      StringProperty(_name),      ObjectProperty[Int](_lrev.toInt), ObjectProperty[Int](_rrev.toInt),
+      StringProperty(_shortdesc), ObjectProperty[Int](_size.toInt), StringProperty(_installed)
+    )
+}
+
+case class TLBackupDisplay(name: StringProperty, rev: StringProperty, date: StringProperty) {
+  def this(_name: String, _rev: String, _date: String) =
+    this(StringProperty(_name), StringProperty(_rev), StringProperty(_date))
+}
+
+case class TLUpdateDisplay(name: StringProperty, var status: StringProperty, lrev: StringProperty, rrev: StringProperty,
+                           shortdesc: StringProperty, size: StringProperty) {
+  def this(_name: String, _status: String, _lrev: String, _rrev: String, _shortdesc: String, _size: String) =
+    this(
+      StringProperty(_name), StringProperty(_status), StringProperty(_lrev), StringProperty(_rrev),
+      StringProperty(_shortdesc), StringProperty(_size)
+    )
+}
